8a1bddea12e6bdaedfa0eea473e495d1bc86e8d5
acheng-osu <chengalb@oregonstate.edu>
2023-01-25 13:08:23 -0800

    started man page

---
+++ main.py
@@ -0,0 +1,6 @@
+from main_menu import display_main_menu
+
+display_main_menu()
+
+
+
---
+++ main_menu.py
@@ -0,0 +1,31 @@
+def display_main_menu():
+    print("""
+    
+    -------------------------------------------------
+    Welcome to the Weather Console App
+    -------------------------------------------------
+
+    Note: please skip to the \"TL;DR\" section if you want to start tinkering right away.
+
+    The purpose of this app is 
+
+
+
+
+
+    -------------------------------------------------
+    TL;DR
+    -------------------------------------------------
+    To search, navigate to the main page and select either 1) Search by City Name or 2) Search by Zip Code.
+    Then follow the instruction prompts so you ensure your input in valid.
+
+    
+    Below are the options available to you at any input field:
+        s) start your search
+        m) go back to the main page
+        h) help menu
+        x) exit the app
+
+
+    
+    """)
