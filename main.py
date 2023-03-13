0e6c05951de4318073996b4dfa8407cc45d02bf9
acheng-osu <chengalb@oregonstate.edu>
2023-03-13 12:35:10 -0700

    improve weather data readability

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from main_menu import display_main_menu
 from help_menu import display_help_menu
 from search import search
 from area_search_by_zip import area_search
+from display_weather_data import display_weather_data
 
 def main():
     display_main_menu()
@@ -10,7 +11,7 @@ def main():
         user_choice = input("Make your selection: ").lower().strip()
 
         if user_choice == "s":
-            print(search())
+            display_weather_data(search())
         elif user_choice == "h":
             display_help_menu()
         elif user_choice == "m":
