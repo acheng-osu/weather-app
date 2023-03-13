d2276d54b87c26a93a007ccdc03ddaa5509ecf49
acheng-osu <chengalb@oregonstate.edu>
2023-03-13 13:55:40 -0700

    handle casing and spacing in searches

--- display_weather_data.py
+++ display_weather_data.py
@@ -1,15 +1,18 @@
 import datetime
-import json
 from geopy.geocoders import Nominatim
+
 
 def display_weather_data(data_dict):
     print("-------------------------------------------------")
-    print("location: " + str(zip_code_from_coords(data_dict["coord"]["lat"], data_dict["coord"]["lon"])))
-    print("coordinates: " + str(data_dict["coord"]["lon"]) + "," + str(data_dict["coord"]["lat"]))
+    print("location: " +
+          str(zip_code_from_coords(data_dict["coord"]["lat"], data_dict["coord"]["lon"])))
+    print("coordinates: " +
+          str(data_dict["coord"]["lon"]) + "," + str(data_dict["coord"]["lat"]))
     print("weather description: " + data_dict["weather"][0]["description"])
     print("temperature (F): " + str(data_dict["main"]["temp"]))
     print("humidity (%): " + str(data_dict["main"]["humidity"]))
-    print("visibility (in meters, max is 10000): " + str(data_dict["visibility"]))
+    print("visibility (in meters, max is 10000): " +
+          str(data_dict["visibility"]))
     print("wind speed (mph): " + str(data_dict["wind"]["speed"]))
     print("cloudiness (%): " + str(data_dict["clouds"]["all"]))
     print("cloudiness (%): " + str(data_dict["clouds"]["all"]))
@@ -17,16 +20,18 @@ def display_weather_data(data_dict):
     print("sunset: " + timestamp_to_string(data_dict["sys"]["sunset"]))
     print("-------------------------------------------------")
 
+
 def timestamp_to_string(secs):
     date_time = datetime.datetime.fromtimestamp(secs)
     date_string = date_time.strftime("%H:%M:%S")
     return date_string
 
+
 def zip_code_from_coords(lat, lon):
     geolocator = Nominatim(user_agent='weather-app')
     location = geolocator.reverse((lat, lon))
     if 'address' in location.raw.keys():
-      if 'postcode' in location.raw['address'].keys():
-        return location.raw['address']['town'], location.raw['address']['state'], location.raw['address']['postcode']
+        if 'postcode' in location.raw['address'].keys():
+            return location.raw['address']['town'], location.raw['address']['state'], location.raw['address']['postcode']
     else:
-      None
+        None
--- search.py
+++ search.py
@@ -3,15 +3,15 @@ from search_by_zip import search_by_zip
 
 def search():
     while True:
-        choice = input("Please choose to either 1) Search by City Name or 2) Search by Zip Code: ")
+        choice = input("Please choose to either 1) Search by City Name or 2) Search by Zip Code: ").strip()
 
         if choice == "1":
             print("Note: additional steps required if there are multiple cities with the same name")
-            city_name = input("Please input the city name: ")
-            state_code = input("Please input the 2 character state code: ")
+            city_name = input("Please input the city name: ").strip().title()
+            state_code = input("Please input the 2 character state code: ").strip().upper()
             return search_by_city(city_name, state_code)
         elif choice == "2":
-            zip_code = input("Please input the 5-digit US zip code: ")
+            zip_code = input("Please input the 5-digit US zip code: ").strip()
             return search_by_zip(int(zip_code))
         elif choice == "x":
             break
